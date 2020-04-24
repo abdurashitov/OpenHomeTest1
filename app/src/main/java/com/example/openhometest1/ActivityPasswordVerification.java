ffa50c18b59cf7bd696f344bba0dd4681945d97b
alex <abdurashitob1999@mail.ru>
2020-04-24 14:58:39 +0300

    1

--- app/src/main/java/com/example/openhometest1/ActivityPasswordVerification.java
+++ app/src/main/java/com/example/openhometest1/ActivityPasswordVerification.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
 
 import br.com.sapereaude.maskedEditText.MaskedEditText;
 
---
+++ app/src/main/java/com/example/openhometest1/User.java
@@ -0,0 +1,18 @@
+package com.example.openhometest1;
+
+import android.provider.ContactsContract;
+
+public class User {
+    String firstName;
+    String last_name;
+    String moderated;
+    String photoUrl;
+    //Contacts contacts;
+    String phoneNumber;
+    String dateAndBirthPlace;
+    String passportSeriesAndNumber;
+    String  passportIssuedBy;
+    String passportIssuedDate;
+    String StringpassportIssuedAddress;
+    //String date	date;
+}
